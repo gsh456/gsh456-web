51c8205209217e5420ac1a7e838b59f58d53aa3c
gsh456 <gsh456@163.com>
2024-06-21 10:58:47 +0800

    初始化2

--- web-home/README.md
+++ web-home/README.md
@@ -1,1 +1,47 @@
-导航项目
+<div align="center">
+<img src="./public/favicon.svg" alt="预览"/>
+
+<h1 align="center">简约风格的网址导航页</h1>
+
+[演示环境](https://www.luode.vip) 
+
+![Snipaste_2023-05-08_09-15-08](public/png/QQ截图20231019212432.png)
+
+</div>
+
+# 启动
+
+## 安装依赖
+```bash
+npm install -g pnpm
+pnpm install
+```
+
+## 启动项目
+```bash
+npm run dev
+```
+
+# docker容器
+
+## latest 版本
+
+```shell
+docker pull luode0320/web-start:latest
+```
+
+启动:
+
+```shell
+docker run -d -p 2000:2000 luode0320/web-start:latest
+```
+
+挂载配置:
+```shell
+docker run -d \
+--restart=always \
+--name web  \
+-p 2000:2000 \
+-v /usr/local/src/web/preset.json:/app/src/preset.json
+luode0320/web-start:latest
+```
---
+++ web-home/vite.config.ts
@@ -0,0 +1,45 @@
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import VueMacros from 'unplugin-vue-macros/vite'
+import Unocss from 'unocss/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import { visualizer } from 'rollup-plugin-visualizer'
+
+export default defineConfig({
+  plugins: [
+    Vue(),
+    Unocss(),
+    Pages(),
+    VueMacros(),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia', '@vueuse/core', 'vue/macros'],
+      dts: 'src/auto-imports.d.ts',
+      dirs: ['src/composables', 'src/stores'],
+      vueTemplate: true,
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()],
+      dirs: ['src/components/**'],
+      extensions: ['vue', 'tsx'],
+      dts: 'src/components.d.ts',
+    }),
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 2000,
+    host: '0.0.0.0',
+  },
+})
